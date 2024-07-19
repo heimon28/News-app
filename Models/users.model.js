675277ee349377eae5230563c9c59d59c60ffc58
heimon1255@gmail.com <heimon28@gmail.com>
2024-07-19 17:57:44 +0530

    change bcrypt to bcryptjs

--- Models/users.model.js
+++ Models/users.model.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
+import bcrypt from 'bcryptjs'
 
 const userSchema = Schema(
   {
