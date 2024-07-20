0cef0594b734b05c192902a5e7abf9dd9d37e3ab
heimon1255@gmail.com <heimon28@gmail.com>
2024-07-20 16:37:49 +0530

    add render url to vitconfig proxcy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'backend-llsz7cpxz-heimon28s-projects.vercel.app'
+      '/api': 'https://news-app-15hj.onrender.com'
 
     }
   },
