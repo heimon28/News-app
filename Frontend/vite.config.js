d5fcb3a2c97568ec1bf1a0442f4805b575468377
heimon1255@gmail.com <heimon28@gmail.com>
2024-07-20 14:08:28 +0530

    add proxcy server from vercel link

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      '/api': 'backend-git-main-heimon28s-projects.vercel.app'
+
     }
   },
   plugins: [react()],
